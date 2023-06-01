4c571d37c470e8676fd8a078771a4b4854fb1c23
walterlv <xblero@gmail.com>
2023-06-01 18:19:05 +0800

    加入 IsExternalInit 包

---
+++ src/dotnetCampus.IsExternalInit.Source/IsExternalInit.cs
@@ -0,0 +1,19 @@
+﻿#if NET5_0_OR_GREATER
+// .NET 5.0 开始已包含 IsExternalInit。
+#else
+// 旧框架需要包含 Nullable Attributes。
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// This dummy class is required to compile records when targeting .NET Standard
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
+#endif
