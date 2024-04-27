2e50f5fda02a3785440679c90f4c19e8c5263585
walterlv <xblero@gmail.com>
2024-04-27 10:48:26 +0800

    Disable ValueTuple warning

--- src/dotnetCampus.LatestCSharpFeatures/ValueTuple/ValueTuple.cs
+++ src/dotnetCampus.LatestCSharpFeatures/ValueTuple/ValueTuple.cs
@@ -1,4 +1,7 @@
-﻿#if NETSTANDARD2_0_OR_GREATER || NETCOREAPP3_0 || NETCOREAPP3_1 || NET5_0_OR_GREATER
+﻿#pragma warning disable CS8600
+#pragma warning disable CS8603
+
+#if NETSTANDARD2_0_OR_GREATER || NETCOREAPP3_0 || NETCOREAPP3_1 || NET5_0_OR_GREATER
 #else
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
