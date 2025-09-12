71a222be9a1a35421ba7f6a103be375cf8763900
walterlv <xblero@gmail.com>
2025-09-12 19:51:31 +0800

    Add deconstruct for KeyValuePair

---
+++ src/DotNetCampus.LatestCSharpFeatures/KeyValuePair/KeyValuePair.g.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+namespace System.Collections.Generic;
+
+#if NETSTANDARD2_1 || NETCOREAPP3_0 || NETCOREAPP3_1 || NET5_0_OR_GREATER
+#else
+/// <summary>
+/// Provide extension methods for <see cref="KeyValuePair{TKey,TValue}"/>.
+/// </summary>
+#if USE_PUBLIC_LATEST_CSHARP_FEATURES
+    public
+#else
+internal
+#endif
+    static class DotNetCampusLatestCSharpFeaturesKeyValuePairExtensions
+{
+    internal static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
+        where TKey : notnull
+    {
+        key = pair.Key;
+        value = pair.Value;
+    }
+}
+#endif
